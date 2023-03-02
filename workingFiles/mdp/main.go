1a5fefa77e4d64345d931d000672d55149ec6f66
Evilolipop <decai.wang@foxmail.com>
2023-03-02 14:23:15 +0800

    add stuff

--- workingFiles/mdp/main.go
+++ workingFiles/mdp/main.go
@@ -4,30 +4,40 @@ import (
 	"bytes"
 	"flag"
 	"fmt"
+	"html/template"
+	"io"
 	"os"
-	"path/filepath"
+	"os/exec"
+	"runtime"
+	"time"
 
 	"github.com/microcosm-cc/bluemonday"
 	"github.com/russross/blackfriday/v2"
 )
 
 const (
-	header = `<!DOCTYPE html>
+	defaultTemplate = `<!DOCTYPE html>
 <html>
  <head>
  <meta http-equiv="content-type" content="text/html; charset=utf-8">
- <title>Markdown Preview Tool</title>
+ <title>{{ .Title }}</title>
  </head>
  <body>
-`
-	footer = `
+{{ .Body }}
  </body>
 </html>
 `
 )
 
+type content struct {
+	Title string
+	Body  template.HTML
+}
+
 func main() {
 	filename := flag.String("file", "", "Markdown file to preview")
+	skipPreview := flag.Bool("s", false, "Skip auto-preview")
+	tFname := flag.String("t", "", "Alternate template file")
 	flag.Parse()
 
 	if *filename == "" {
@@ -35,39 +45,107 @@ func main() {
 		os.Exit(1)
 	}
 
-	if err := run(*filename); err != nil {
+	if err := run(*filename, *tFname, os.Stdout, *skipPreview); err != nil {
 		fmt.Fprintln(os.Stderr, err)
 		os.Exit(1)
 	}
 }
 
-func run(filename string) error {
+func run(filename, tFname string, out io.Writer, skipPreview bool) error {
 	input, err := os.ReadFile(filename)
 	if err != nil {
 		return err
 	}
 
-	htmlData := parseContent(input)
+	htmlData, err := parseContent(input, tFname)
+	if err != nil {
+		return err
+	}
 
-	outName := fmt.Sprintf("%s.html", filepath.Base(filename))
-	fmt.Println(outName)
+	temp, err := os.CreateTemp("", "mdp*.html")
+	if err != nil {
+		return err
+	}
+	if err := temp.Close(); err != nil {
+		return err
+	}
 
-	return saveHtml(outName, htmlData)
+	outName := temp.Name()
+
+	fmt.Fprintln(out, outName)
+
+	if err := saveHtml(outName, htmlData); err != nil {
+		return err
+	}
+
+	if skipPreview {
+		return nil
+	}
+
+	defer os.Remove(outName)
+
+	return preview(outName)
 }
 
-func parseContent(input []byte) []byte {
+func parseContent(input []byte, tFname string) ([]byte, error) {
 	output := blackfriday.Run(input)
 	body := bluemonday.UGCPolicy().SanitizeBytes(output)
 
+	t, err := template.New("mdp").Parse(defaultTemplate)
+	if err != nil {
+		return nil, err
+	}
+
+	if tFname != "" {
+		t, err = template.ParseFiles(tFname)
+		if err != nil {
+			return nil, err
+		}
+	}
+
+	c := content{
+		Title: "Markdown Preview Tool",
+		Body:  template.HTML(body),
+	}
+
 	var buffer bytes.Buffer
 
-	buffer.WriteString(header)
-	buffer.Write(body)
-	buffer.WriteString(footer)
+	if err := t.Execute(&buffer, c); err != nil {
+		return nil, err
+	}
 
-	return buffer.Bytes()
+	return buffer.Bytes(), nil
 }
 
 func saveHtml(filename string, data []byte) error {
 	return os.WriteFile(filename, data, 0o644)
+}
+
+func preview(filename string) error {
+	cName := ""
+	cParams := []string{}
+
+	switch runtime.GOOS {
+	case "linux":
+		cName = "xdg-open"
+	case "windows":
+		cName = "cmd.exe"
+	case "darwin":
+		cName = "open"
+	default:
+		return fmt.Errorf("unsupported OS: %s", runtime.GOOS)
+	}
+
+	cParams = append(cParams, filename)
+
+	cPath, err := exec.LookPath(cName)
+	if err != nil {
+		return err
+	}
+
+	err = exec.Command(cPath, cParams...).Run()
+
+	time.Sleep(2 * time.Second)
+
+	return err
 }
