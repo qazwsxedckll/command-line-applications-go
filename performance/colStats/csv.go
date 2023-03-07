5393d13b8a3f527d8b4650d10d1df67d8ccf97a9
Evilolipop <decai.wang@foxmail.com>
2023-03-07 18:03:42 +0800

    add stuff

--- performance/colStats/csv.go
+++ performance/colStats/csv.go
@@ -23,16 +23,22 @@ func avg(data []float64) float64 {
 
 func csv2float(r io.Reader, column int) ([]float64, error) {
 	cr := csv.NewReader(r)
+	cr.ReuseRecord = true
 
 	column--
 
-	allData, err := cr.ReadAll()
-	if err != nil {
-		return nil, fmt.Errorf("cannot read data from file: %w", err)
-	}
-
 	var data []float64
-	for i, row := range allData {
+
+	for i := 0; ; i++ {
+		row, err := cr.Read()
+		if err == io.EOF {
+			break
+		}
+
+		if err != nil {
+			return nil, fmt.Errorf("cannot read data from file: %w", err)
+		}
+
 		if i == 0 {
 			continue
 		}
@@ -48,5 +54,28 @@ func csv2float(r io.Reader, column int) ([]float64, error) {
 
 		data = append(data, v)
 	}
+
+	// allData, err := cr.ReadAll()
+	// if err != nil {
+	// 	return nil, fmt.Errorf("cannot read data from file: %w", err)
+	// }
+
+	// var data []float64
+	// for i, row := range allData {
+	// 	if i == 0 {
+	// 		continue
+	// 	}
+
+	// 	for len(row) <= column {
+	// 		return nil, fmt.Errorf("%w: File has only %d columns", ErrInvalidColumn, len(row))
+	// 	}
+
+	// 	v, err := strconv.ParseFloat(row[column], 64)
+	// 	if err != nil {
+	// 		return nil, fmt.Errorf("%w: %s", ErrNotNumber, err)
+	// 	}
+
+	// 	data = append(data, v)
+	// }
 	return data, nil
 }
--- performance/colStats/go.mod
+++ performance/colStats/go.mod
@@ -1,3 +1,5 @@
 module colStats
 
 go 1.20
+
+require golang.org/x/tools v0.6.0 // indirect
